bc70b80e4727d7fe445718a60a9b2d212e89dae6
yashjawale <yash25.j@gmail.com>
2023-05-23 09:35:12 +0530

    Updated metadata

--- slidesync-web/vite.config.js
+++ slidesync-web/vite.config.js
@@ -8,6 +8,22 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest: {
+        name: "SlideSync Mobile",
+        short_name: "SlideSync",
+        shortcuts: [
+          {
+            name: "Scan QR Code",
+            short_name: "Scan",
+            description: "Scan QR Code from SlideSync Desktop",
+            url: "/scan",
+          },
+          {
+            name: "Enter Code Manually",
+            short_name: "Enter Code",
+            description: "Enter SlideSync Desktop code manually",
+            url: "/codeentry",
+          },
+        ],
         icons: [
           // {
           //   src: "/icons/manifest-icon-512.maskable.png",
@@ -15,6 +31,12 @@ export default defineConfig({
           //   type: "image/png",
           //   purpose: "any maskable",
           // },
+          {
+            src: "icons/slidesync.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "maskable any",
+          },
           {
             src: "icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
